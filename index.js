d890fdd33f288cd7fac6b7ec0dcbc2f1a57e6ee5
quando1910 <dhquan1910@gmail.com>
2020-08-30 23:14:54 +0700

    first commit

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
+const { countReset } = require('console');
+
 var app = require('express')();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
-var port = process.env.PORT || 3000;
+var port = process.env.PORT || 4000;
+// var cors = require('cors')
+ 
+// app.use(cors())
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/index.html');
