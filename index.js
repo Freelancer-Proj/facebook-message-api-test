6a362f0950530ab3915d1614c1bdff7347c8482c
st-quando <quan.do@supremetech.vn>
2020-09-15 11:10:48 +0700

    try

--- index.js
+++ index.js
@@ -17,13 +17,30 @@ app.get('/webhook', (req, res) => {
 })
 
 app.post('/webhook', middleware(config), (req, res) => {
-  const event = req.body.events[0];
   console.log(JSON.stringify(req.body));
-  return client.replyMessage(event.replyToken, {
-    type: 'text',
-    text: 'answer'
-  });   
+  
+  Promise
+    .all(req.body.events.map(handleEvent))
+    .then((result) => res.json(result))
+    .catch((err) => {
+      console.error(err);
+      res.status(500).end();
+    });
 })
+
+// event handler
+function handleEvent(event) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+  // ignore non-text-message event
+  return Promise.resolve(null);
+  }
+
+  // create a echoing text message
+  const echo = { type: 'text', text: event.message.text };
+
+  // use reply API
+  return client.replyMessage(event.replyToken, echo);
+}
 
 // app.use((err, req, res, next) => {
 //   if (err instanceof SignatureValidationFailed) {
